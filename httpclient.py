df242b431a749589e1a9471b1e8853db5df5d1c3
philiponions <hueycoy9@gmail.com>
2023-10-11 22:16:47 -0600

    draft

--- httpclient.py
+++ httpclient.py
@@ -23,6 +23,7 @@ import socket
 import re
 # you may use urllib to encode data appropriately
 import urllib.parse
+import json
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -40,14 +41,16 @@ class HTTPClient(object):
         self.socket.connect((host, port))
         return None
 
-    def get_code(self, data):
-        return None
+    def get_code(self, data):             
+        return int([i for i in data.split(" ")][1])
 
     def get_headers(self,data):
         return None
 
     def get_body(self, data):
-        return None
+        body_index = data.find('\r\n\r\n')
+        # print(body_index)
+        return data[body_index+4:]
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -59,25 +62,108 @@ class HTTPClient(object):
     def recvall(self, sock):
         buffer = bytearray()
         done = False
-        while not done:
-            part = sock.recv(1024)
+        while not done:            
+            part = sock.recv(1024)    
+            print(part)
             if (part):
                 buffer.extend(part)
             else:
-                done = not part
+                done = not part            
         return buffer.decode('utf-8')
 
-    def GET(self, url, args=None):
+    def GET(self, url, args=None):              
+    
         code = 500
-        body = ""
+        try:
+
+            o = urllib.parse.urlparse(url)
+            scheme = o.scheme
+            
+            port = o.port if o.port else 80
+            host = o.hostname
+            path = o.path if "/" in o.path else "/"
+
+            print("scheme:", scheme)
+            print("host:", host)
+            print("port:", port)
+            print("path:", path)
+            self.connect(host, port)
+
+        # TODO: use urllib.parse
+        
+            message = f"GET {path} HTTP/1.1\r\n" \
+                      f"Host: {host}\r\n" \
+                      f"Connection: close\r\n" \
+                      f"\r\n"
+                      
+            print("Message:")
+            print(message)
+            self.sendall(message)            
+            body = self.recvall(self.socket)          
+            self.close()
+            # code = self.get_code(body)
+            print("body:")
+            print(body)        
+            body = str(body)    
+            code = self.get_code(body)                
+            body = self.get_body(body)
+        except Exception as e:     
+            print(e)      
+            body = ""
+        
+        print(code)
+        print(body)
+        
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
         code = 500
         body = ""
+        
+        o = urllib.parse.urlparse(url)
+
+        port = o.port
+        host = o.hostname
+        path = o.path
+        
+        post_data = ""  # Replace with your data
+        
+        if args:
+            for i, (key, value) in enumerate(args.items()):
+                post_data += str(key) + "="
+                post_data += str(value)
+                if (i < len(args) - 1):
+                    post_data += "&"
+
+        self.connect(host, port)
+
+        message =f"POST {path} HTTP/1.1\r\n" \
+                f"Host: {host} \r\n" \
+                f"Content-Type: application/x-www-form-urlencoded\r\n" \
+                f"Content-Length: {len(post_data)}\r\n" \
+                f"\r\n" \
+                f"{post_data}"
+
+        try:
+            self.sendall(message)        
+            self.socket.shutdown(socket.SHUT_WR)        
+            body = self.recvall(self.socket)
+            self.close()
+            code = self.get_code(body)   
+            body = self.get_body(body)
+        
+        except Exception as e:            
+            print(e)
+            body  = b""
+        
+        
+        print("result:")
+        print(code)
+        print(body)
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
+        print(url)
         if (command == "POST"):
             return self.POST( url, args )
         else:
